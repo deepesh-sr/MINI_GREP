6cd6e9b532109bd2e7b5c6f211c9e9cbc9e46e38
deepesh-sr <220714100078@cutm.ac.in>
2025-05-22 02:02:13 +0530

    made logic more idiomatic :)

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,33 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_patch = &args[2];
+    let config= Config::new(&args);
+    println!("Search for - {}", config.query);
+    println!("The file targeted - {}",config.target_file);
 
-    println!("Search for - {}", query);
-    println!("The file targeted - {file_patch}");
 
     // read the file
     // import the fs
 
-    let content = fs::read_to_string("story.txt").expect("Must been able to read the file");
+    // let content = fs::read_to_string("story.txt").expect("Must been able to read the file");
+    let content = fs::read_to_string(config.target_file).expect("Must been able to read the file");
+    
 
     println!(
         "With text:\n{content}"
     );
 }
+
+struct Config {
+    query : String,
+    target_file :String
+}
+impl Config {
+ fn new(args: &[String])->Config{
+        let query = args[1].clone();
+        let target_file = args[2].clone();
+        Config { query, target_file }
+    }   
+   
+}
+ 
