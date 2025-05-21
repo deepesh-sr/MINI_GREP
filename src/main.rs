a6bf0484890d82c1329aff2215ec95be68d58e15
deepesh-sr <220714100078@cutm.ac.in>
2025-05-22 01:04:15 +0530

    file reading and argument collection added :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    //iterators
+    // iterators produce a series of values
+    // we can call .collect to convert it into an collection like vector
+
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_patch = &args[2];
+
+    println!("Search for - {}", query);
+    println!("The file targeted - {file_patch}");
+
+    // read the file
+    // import the fs
+
+    let content = fs::read_to_string("story.txt").expect("Must been able to read the file");
+
+    println!(
+        "With text:\n{content}"
+    );
 }
