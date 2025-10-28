7d0c60e6059ee66408212e975ef44fe1479cb827
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-10-28 18:43:36 +0530

    rebuild

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,8 @@
-use std::{env, process};
-use minigrep::Config;
-fn main() {
-    //iterators
-    // iterators produce a series of values
-    // we can call .collect to convert it into an collection like vector
+use std::env;
 
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments : {err}");
-        process::exit(1);
-    });
-    println!("Search for - {}", config.query);
-    println!("The file targeted - {}", config.target_file);
-
-    if let Err(e) = minigrep::run(config){
-        print!("Application Error : {e}");
-        process::exit(1);
-    }
-
-    // read the file
-    // import the fs
+//if you program arg could contain invalid unicode, use std::env::arg_os
+// it return OsStrings instead of string
+fn main (){
+    let args : Vec<String> = env::args().collect();
+    dbg!(args);
 }
-
-
-
-
