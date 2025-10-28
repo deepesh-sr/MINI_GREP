d124a84759e17a3befef6be8282d6cd992178591
deepesh-sr <rathoredeepeshsingh@gmail.com>
2025-10-28 18:49:47 +0530

    basic arg and file reading added

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::env;
+use std::{env, fs};
 
 //if you program arg could contain invalid unicode, use std::env::arg_os
 // it return OsStrings instead of string
 fn main (){
     let args : Vec<String> = env::args().collect();
-    dbg!(args);
+    
+    let word_to_be_searched = &args[1];
+    let file_path = &args[2];
+
+    let file_content = fs::read_to_string(file_path).expect("Should have been able to read to file");
+
+    println!("Contents of file : {}",file_content)
+
+
 }
